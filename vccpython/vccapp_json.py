a1497f8ad40a11d7903f3c8c338c5eb9999b0956
thermalogic <cmh@seu.edu.cn>
2021-03-23 12:22:42 +0800

    json example

---
+++ vccpython/vccapp_json.py
@@ -0,0 +1,39 @@
+
+"""
+General Object-oriented Abstraction  of VP Cycle 
+The Simulator of VC Cycle 
+  * Input :the json file of the cycle model
+  * Output: text file
+Run: 
+   python vccapp_json.py
+"""
+from vcc.vccobj import VCCycle
+from vcc.utils import OutFiles
+from platform import os
+
+# json
+import json
+def create_dictcycle_from_jsonfile(filename):
+    """ create dict cycle from json file"""
+    with open(filename, 'r') as f:
+        dictcycle = json.loads(f.read())
+
+    # convection json to the tuple in python
+    for itemtuples in dictcycle["connectors"]:
+        for i in range(len(itemtuples)):
+            keys = list(itemtuples[i].keys())
+            itemtuples[i] = (itemtuples[i][keys[0]], itemtuples[i][keys[1]])
+    return dictcycle
+
+curpath = os.path.abspath(os.path.dirname(__file__))
+rankinefilename = curpath+'\\'+'./jsonmodel/ivcr723.json'
+thedictcycle = create_dictcycle_from_jsonfile(rankinefilename)
+# the simulator
+cycle = VCCycle(thedictcycle)
+cycle.simulator()
+# output to console
+OutFiles(cycle)
+# output to the file
+ResultFilePath = curpath+'/result/'
+ResultFileName = ResultFilePath+thedictcycle['name']
+OutFiles(cycle, ResultFileName + '.txt')
